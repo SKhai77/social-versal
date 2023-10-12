e49c4c6ddedc7a02758a79ac6e1799ca485fc6fe
SKhai77 <orionkhai@gmail.com>
2023-10-11 22:11:56 -0500

    Added comment to the file

--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
-const User = require('./User');
-const Thought = require('./Thought');
+// Importing the User and Thought models
+const User = require("./User");
+const Thought = require("./Thought");
 
+// Exporting an object containing the User and Thought models
 module.exports = { User, Thought };
