afdf6fae9b40e7ead3b9234453d720d68672d0fd
SKhai77 <orionkhai@gmail.com>
2023-10-12 00:33:38 -0500

    Added all the required routes to the file

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,10 +1,12 @@
 // Importing the express library and creating a router instance
 const router = require("express").Router();
 // Importing controller functions for handling user-related operations
-const { getUsers, createUser } = require("../../controllers/userController");
+const { getUsers, getSingleUser, createUser, updateUser, deleteUser, addFriend, removeFriend } = require("../../controllers/userController");
 
-// Defining routes for handling GET and POST requests at the root ("/") endpoint
+// Defining routes for handling GET, POST, PUT, and DELETE requests at the root ("/") endpoint
 router.route("/").get(getUsers).post(createUser);
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route("/:userId/friends").post(addFriend).delete(removeFriend);
 
 // Exporting the router
 module.exports = router;
